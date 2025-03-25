d8ec65d0e67f419ef6ca908068230de444a90dae
Qwexaro <Sergikstar25.gold@mail.ru>
2025-03-25 14:01:17 +0300

    main

--- src/main/java/CoolNumbers.java
+++ src/main/java/CoolNumbers.java
@@ -11,9 +11,11 @@ public class CoolNumbers {
         for (int i = 0; i < 2_000_000; i++) {
             String letters = "А, В, Е, К, М, Н, О, Р, С, Т, У, Х";
             String[] arrayLetters = letters.split(", ");
-            
+
             int indexRandomLetters = (int) (Math.random() * 12);
             String randomLetters = arrayLetters[indexRandomLetters];
+
+            int randomNumber = (int) (Math.random() * 10);
         }
 
 
