b4282eaf58547404d1842ac8b9d9d7fadc07dc53
Qwexaro <Sergikstar25.gold@mail.ru>
2025-03-25 13:50:14 +0300

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,29 +2,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    List<String> names = new ArrayList<>() {{
-        add("Артем");
-        add("Балван");
-        add("Вошан");
-        add("Гена");
-        add("Дима");
-        add("Ержан");
-        add("Жан");
-    }};
+    public static void main(String[] args) {
 
-    public static int algorithmBinarySearch(String searchName,  List<String> listNames) {
+        List<String> names = new ArrayList<>() {{
+            add("Артем");
+            add("Балван");
+            add("Влангус");
+            add("Гена");
+            add("Дима");
+            add("Ержан");
+            add("Жан");
+        }};
+
+        System.out.println(algorithmBinarySearch("Влангус", names));
+    }
+
+    public static int algorithmBinarySearch(String searchName, List<String> listNames) {
         int start = 0;
         int end = listNames.size() - 1;
         int middle = (start + end) / 2;
 
-        while (searchName.compareTo(listNames.get(middle))!=0){
-            if(searchName.compareTo(listNames.get(middle))< 0){
+        while (searchName.compareTo(listNames.get(middle)) != 0) {
+            if (searchName.compareTo(listNames.get(middle)) < 0) {
                 end = middle - 1;
             } else if (searchName.compareTo(listNames.get(middle)) > 0) {
                 start = start + 1;
             }
+            middle = (start + end) / 2;
         }
-        
+
         return middle;
     }
 }
