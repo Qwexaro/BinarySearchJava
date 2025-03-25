c40c7a75bd8641a9db081b646593e6bc29379706
Qwexaro <Sergikstar25.gold@mail.ru>
2025-03-25 14:10:30 +0300

    main

--- src/main/java/CoolNumbers.java
+++ src/main/java/CoolNumbers.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class CoolNumbers {
     public static void main(String[] args) {
-
+        System.out.println(generateCoolNumbers());
     }
 
     public static ArrayList<String> generateCoolNumbers() {
