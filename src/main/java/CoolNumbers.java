1187ec9d2753fc54ede29cee58d1fe5c9ff26693
Qwexaro <Sergikstar25.gold@mail.ru>
2025-03-26 12:12:51 +0300

    main

--- src/main/java/CoolNumbers.java
+++ src/main/java/CoolNumbers.java
@@ -25,7 +25,13 @@ public class CoolNumbers {
 
 
     public static void getIndexSearchNameBinary(String searchAutoNumber, ArrayList<String> listCoolNumbers){
-        
+        int start = 0;
+        int end = listCoolNumbers.size() - 1;
+        int middle = (start + end) / 2;
+
+        while (searchAutoNumber.compareTo(listCoolNumbers.get(middle)) != 0){
+            
+        }
     }
 
 
